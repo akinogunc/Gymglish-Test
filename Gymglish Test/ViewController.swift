0b928eab780e1de2aa2cf1d6101eedfcaab810ae
Akın <akinogunc@gmail.com>
2019-06-03 17:09:51 +0200

    Initial Commit

---
+++ Gymglish Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gymglish Test
+//
+//  Created by AKIN on 3.06.2019.
+//  Copyright © 2019 AKIN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
